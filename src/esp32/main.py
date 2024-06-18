35e9db7854f02a603158029f921e8cf1de9a195d
Bastian <badmaxyt@gmail.com>
2024-06-18 11:21:18 +0200

    feat: :sparkles: add SoftwareSerial with UART

---
+++ src/esp32/main.py
@@ -0,0 +1,17 @@
+from machine import UART, Pin
+import time
+
+# Configure UART
+uart = UART(1, baudrate=9600, tx=17, rx=16)  # Use GPIO 17 as TX and GPIO 16 as RX
+
+# Function to read data from UART
+def read_uart():
+    if uart.any():
+        data = uart.readline()
+        if data:
+            print(data.decode('utf-8').strip())
+
+# Main loop
+while True:
+    read_uart()
+    time.sleep(1)
--- src/iot/iot.ino
+++ src/iot/iot.ino
@@ -1,22 +1,27 @@
 #include "libraries/ButtonModule/ButtonModule.h"
 #include "libraries/LightSensor/LightSensor.h"
 #include "libraries/TH02Module/TH02Module.h"
-#include "libraries/TH02Module/TH02Module.h"
 #include "Arduino.h"
+#include <SoftwareSerial.h>
 
 #define buttonRedPin 5
 #define buttonGreenPin 4
 #define LIGHT_SENSOR_PIN A3
 #define SERIAL_BAUD_RATE 9600
+#define RX_PIN 10
+#define TX_PIN 11
 
 ButtonModule buttonRed(buttonRedPin);
 ButtonModule buttonGreen(buttonGreenPin);
 LightSensorModule lightSensor(LIGHT_SENSOR_PIN);
 TH02Module th02Sensor;
 
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // RX, TX
+
 void setup()
 {
-    Serial.begin(SERIAL_BAUD_RATE); // Start serial communication
+    Serial.begin(SERIAL_BAUD_RATE);   // Start serial communication
+    mySerial.begin(SERIAL_BAUD_RATE); // Start software serial communication
     buttonRed.init();
     buttonGreen.init();
     lightSensor.init();
@@ -52,6 +57,7 @@ void loop()
     float temperature = th02Sensor.getTemperature();
     float humidity = th02Sensor.getHumidity();
 
+    // Print to serial monitor
     Serial.print("Temperature: ");
     Serial.print(temperature);
     Serial.println("C");
@@ -63,6 +69,19 @@ void loop()
     Serial.print("Light level: ");
     Serial.print(lightLevel);
     Serial.println("%");
+
+    // Send data to ESP32
+    mySerial.print("Temperature: ");
+    mySerial.print(temperature);
+    mySerial.println("C");
+
+    mySerial.print("Humidity: ");
+    mySerial.print(humidity);
+    mySerial.println("%");
+
+    mySerial.print("Light level: ");
+    mySerial.print(lightLevel);
+    mySerial.println("%");
 
     delay(1000); // Wait for 1 second before next reading
 }
